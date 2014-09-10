6018b96cc4b8bc4489ac9d37c0c1741faf386fcf
dougjuliao <douglas_juliao16@hotmail.com>
2014-09-10 17:42:35 -0300

    Upload de arquivos
    Primeiro commit, upload dos arquivos.
    Calculadora feita em html,css e javascript

---
+++ script.js
@@ -0,0 +1,34 @@
+/*-----------------------------------------CALCULADORA----------------------------------------------------------*/
+// pego os elementos li e div input
+var btn 	  = document.querySelectorAll(".key li"),
+	input 	  = document.querySelector(".input-valor"),
+	operador  = document.querySelectorAll(".operador");
+
+// Percorro o array para usar todas as informações
+for(var i = 0; i < btn.length; i++){
+	btn[i].addEventListener('click',function(){
+		var btnVal 	 = this.innerHTML,
+			inputVal = input.innerHTML;
+		//console.log(btnVal);
+
+		// crio um clear caso o c for clicado
+		switch (btnVal){
+			case "c":
+				input.innerHTML = "";
+				break;
+			case "=":
+				// Crio a variável de equação aqui eu utilizo a função eval do javascript
+				var equacao = inputVal;
+				
+				if(equacao){
+					input.innerHTML = eval(equacao);
+				}
+				break;
+			default:
+				input.innerHTML += btnVal;
+				break;	
+		}
+
+	});
+}
+
