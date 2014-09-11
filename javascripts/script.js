fcdee9848659aebfbf9f4050b86a1cf08f1405d3
dougjuliao <douglas_juliao16@hotmail.com>
2014-09-11 11:18:47 -0300

    Adicionado esquema de keypress
    Agora a calculadora pode ser usada com as teclas
    :)

--- javascripts/script.js
+++ javascripts/script.js
@@ -6,6 +6,51 @@ var btn 	  = document.querySelectorAll(".key li"),
 
 // Percorro o array para usar todas as informações
 for(var i = 0; i < btn.length; i++){
+	document.onkeypress = function(){
+		var key = event.keyCode;
+		//console.log(key);
+		for(var e = 0; e <= 10; e++){
+			if(key === (48+e)){
+				input.innerHTML += e;
+			}
+		}
+		switch (key){
+			case 42:
+				input.innerHTML += "*";
+				break;
+			case 43:
+				input.innerHTML += "+";
+				break;
+			case 45:
+				input.innerHTML += "-";
+				break;
+			case 46:
+				input.innerHTML += ".";
+				break;
+			case 47:
+				input.innerHTML += "/";
+				break;
+			case 13:
+			case 61:
+				var equacao = input.innerHTML;
+				if(equacao){
+					try {
+						input.innerHTML = eval(equacao);
+					} catch (e) {
+						alert('Erro na expressão');
+
+					} 
+				}
+				break;
+			case 67:
+			case 99:
+				input.innerHTML = "";
+				break;						
+			default:
+				//console.log("Procurando erros?");
+				break;
+		}
+	};
 	btn[i].addEventListener('click',function(){
 		var btnVal 	 = this.innerHTML,
 			inputVal = input.innerHTML;
